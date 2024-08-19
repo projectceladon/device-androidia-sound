d36e26ecb10aaa0aefb70b0fa7fe745fb8eaac99
sgnanase <sundar.gnanasekaran@intel.com>
2024-08-19 23:31:04 +0530

    Add in_read_bt initial changes
    Compiles fine
    
    Signed-off-by: sgnanase <sundar.gnanasekaran@intel.com>

--- primary/audio_hw.c
+++ primary/audio_hw.c
@@ -503,84 +503,9 @@ static ssize_t in_read(struct audio_stream_in *stream, void* buffer,
 
 //[BT SCO VoIP Call
     if(adev->in_sco_voip_call) {
-        /* VoIP pcm read from bt alsa card */
-        size_t frames_out = round_to_16_mult(in->pcm_config->period_size);
-        size_t frames_in = round_to_16_mult(bt_in_config.period_size);
-        size_t buf_size_out = in->pcm_config->channels * frames_out * SAMPLE_SIZE_IN_BYTES;
-        size_t buf_size_in = bt_in_config.channels * frames_in * SAMPLE_SIZE_IN_BYTES;
-        size_t buf_size_remapped = in->pcm_config->channels * frames_in * SAMPLE_SIZE_IN_BYTES;
-        int16_t *buf_out = (int16_t *) malloc (buf_size_out);
-        int16_t *buf_in = (int16_t *) malloc (buf_size_in);
-        int16_t *buf_remapped = (int16_t *) malloc (buf_size_remapped);
-
-        if(adev->voip_in_resampler == NULL) {
-            int ret = create_resampler(bt_in_config.rate /*src rate*/, in->pcm_config->rate /*dst rate*/, in->pcm_config->channels/*dst channels*/,
-                        RESAMPLER_QUALITY_DEFAULT, NULL, &(adev->voip_in_resampler));
-            ALOGV("%s : bytes_requested : %zu", __func__, bytes);
-            ALOGV("%s : frames_in %zu frames_out %zu",__func__, frames_in, frames_out);
-            ALOGD("%s : size_in %zu size_out %zu size_remapped %zu", __func__, buf_size_in, buf_size_out, buf_size_remapped);
-            if (ret != 0) {
-                adev->voip_in_resampler = NULL;
-                ALOGE("%s : Failure to create resampler %d", __func__, ret);
-
-                free(buf_in);
-                free(buf_out);
-                free(buf_remapped);
-                goto exit;
-            } else {
-                ALOGD("%s : voip_in_resampler created rate : [%d -> %d]", __func__, bt_in_config.rate, in->pcm_config->rate);
-            }
-        }
-
-        memset(buf_in, 0, buf_size_in);
-        memset(buf_remapped, 0, buf_size_remapped);
-        memset(buf_out, 0, buf_size_out);
-
-        ret = pcm_read(in->pcm, buf_in, buf_size_in);
-
-#ifdef DEBUG_PCM_DUMP
-        if(sco_call_read != NULL) {
-            fwrite(buf_in, 1, buf_size_in, sco_call_read);
-        } else {
-            ALOGD("%s : sco_call_read was NULL, no dump", __func__);
-        }
-#endif
-        adjust_channels(buf_in, bt_in_config.channels, buf_remapped, in->pcm_config->channels, 
-                                        SAMPLE_SIZE_IN_BYTES, buf_size_in);
-
-        //ALOGV("%s : remapping : [%d -> %d]", __func__, bt_in_config.channels, in->pcm_config->channels);
-
-#ifdef DEBUG_PCM_DUMP
-        if(sco_call_read_remapped != NULL) {
-            fwrite(buf_remapped, 1, buf_size_remapped, sco_call_read_remapped);
-        } else {
-            ALOGD("%s : sco_call_read_remapped was NULL, no dump", __func__);
-        }
-#endif
-
-        if(adev->voip_in_resampler != NULL) {
-            adev->voip_in_resampler->resample_from_input(adev->voip_in_resampler, (int16_t *)buf_remapped, (size_t *)&frames_in, (int16_t *) buf_out, (size_t *)&frames_out);
-            //ALOGV("%s : upsampling [%d -> %d]",__func__, bt_in_config.rate, in->pcm_config->rate);
-        }
-
-        ALOGV("%s : modified frames_in %zu frames_out %zu",__func__, frames_in, frames_out);
-
-        buf_size_out = in->pcm_config->channels * frames_out * SAMPLE_SIZE_IN_BYTES;
-        bytes = buf_size_out;
-
-#ifdef DEBUG_PCM_DUMP
-        if(sco_call_read_bt != NULL) {
-            fwrite(buf_out, 1, buf_size_out, sco_call_read_bt);
-        } else {
-            ALOGD("%s : sco_call_read_bt was NULL, no dump", __func__);
-        }
-#endif
-
-        memcpy_s(buffer,buf_size_out, buf_out, buf_size_out);
-
-        free(buf_in);
-        free(buf_out);
-        free(buf_remapped);
+        ret = in_read_bt(in, adev, buffer, bytes);
+	if (ret != 0)
+            goto exit;
 //BT SCO VoIP Call]
     } else {
         /* pcm read for primary card */
